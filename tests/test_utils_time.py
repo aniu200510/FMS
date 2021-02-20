5d2eefc7a93c1441f09021dfd9fed28f39d98397
zyli <zyli@cfiec.net>
2021-02-20 10:15:27 +0800

    test:Optimize the test case format.

--- tests/test_utils_time.py
+++ tests/test_utils_time.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
-from utils.time import ZTime
-from django.test import TestCase
 from datetime import datetime
 
+from django.test import TestCase
+
+from utils.time import ZTime
 
 
 class ZTimeTests(TestCase):
-    
+
     def setUp(self):
         self.ztime = ZTime(datetime(2021, 2, 2))
 
@@ -17,28 +18,27 @@ class ZTimeTests(TestCase):
     def test_week_end(self):
         self.assertEqual(self.ztime.week_end,
                          datetime(2021, 2, 7))
-    
+
     def test_month_start(self):
         self.assertEqual(self.ztime.month_start,
                          datetime(2021, 2, 1))
-    
+
     def test_month_end(self):
         self.assertEqual(self.ztime.month_end,
                          datetime(2021, 2, 28))
-    
+
     def test_quarter_start(self):
         self.assertEqual(self.ztime.quarter_start,
                          datetime(2021, 1, 1))
-    
+
     def test_quarter_end(self):
         self.assertEqual(self.ztime.quarter_end,
                          datetime(2021, 3, 31))
-    
+
     def test_year_start(self):
         self.assertEqual(self.ztime.year_start,
                          datetime(2021, 1, 1))
-    
+
     def test_year_end(self):
         self.assertEqual(self.ztime.year_end,
                          datetime(2021, 12, 31))
-        
