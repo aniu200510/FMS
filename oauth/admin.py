b0469d78f8e2ae6746a90a8d5232b41af7b64a11
zyli <zyli@cfiec.net>
2021-02-20 10:23:31 +0800

    feat:Add FundNet model and FundTrade model.

--- oauth/admin.py
+++ oauth/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
-from oauth.models import Fund, FundAccount, FundCompany, FundManager, User
+from oauth.models import (Fund, FundAccount, FundCompany, FundManager, FundNet,
+                          FundTrade, User)
 
 admin.site.register(User)
 admin.site.register(FundCompany)
 admin.site.register(FundManager)
 admin.site.register(Fund)
 admin.site.register(FundAccount)
+admin.site.register(FundTrade)
+admin.site.register(FundNet)
--- oauth/models.py
+++ oauth/models.py
@@ -100,3 +100,58 @@ class FundAccount(models.Model):
         verbose_name = _('fund account')
         db_table = 'fund_account'
         ordering = ['-date']
+
+
+class FundTrade(models.Model):
+    date = models.DateField(
+        _('date'), help_text='日期')
+    commission = models.FloatField(
+        _('commission'), help_text='手续费')
+    share = models.FloatField(
+        _('fund share'), help_text='基金份额')
+    principal = models.FloatField(
+        _('principal'), help_text='本金')
+    amount = models.FloatField(
+        _('amount'), help_text='金额')
+    fund = models.ForeignKey(
+        Fund,
+        related_name='ref_fund_by_fund_trade',
+        default=None,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT)
+
+    def __str__(self):
+        fmt_str = ''
+        if self.fund:
+            fmt_str = str(self.date) + ' ' + self.fund.name
+
+        return fmt_str
+
+    class Meta:
+        verbose_name = _('fund trade')
+        db_table = 'fund_trade'
+        ordering = ['-date']
+
+
+class FundNet(models.Model):
+    date = models.DateField(
+        _('date'), help_text='日期')
+    nav = models.FloatField(
+        _('NAV'), help_text='单位净值', default=0.0)
+    accnav = models.FloatField(
+        _('ACCNAV'), help_text='累计净值', default=0.0)
+    naps = models.FloatField(
+        _('NAPS'), help_text='日增长率', default=0.0)
+    fund = models.ForeignKey(
+        Fund,
+        related_name='ref_fund_by_fund_net',
+        default=None,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = _('fund net')
+        db_table = 'fund_net'
+        ordering = ['-date']
