12222178009eb761a3d321ee605ae0a130230283
zyli <zyli@cfiec.net>
2021-03-11 10:00:38 +0800

    feat:Add scheduled tasks.

--- fms/config.py
+++ fms/config.py
@@ -1,3 +1,4 @@
+from celery.schedules import crontab
 
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/10'
 BROKER_URL = 'redis://127.0.0.1:6379/11'
@@ -11,3 +12,17 @@ CELERY_TASK_RESULT_EXPIRES = 60 * 60
 CELERY_TIMEZONE = 'Asia/Shanghai'
 
 CELERYBEAT_SCHEDULE = {}
+
+# timedelta 和 crontab 各有不同应用场景
+CELERYBEAT_SCHEDULE = {
+    'check-ban-policy': {
+        'task': 'das.tasks.save_fund_net_every_day',
+        'schedule': crontab(hour='*/1', minute=0),
+        'args': (1, ),  # 第一个参数必须和crontab的hour参数间隔时长一致
+    },
+    'save-core-indicator-every-hour': {
+        'task': 'CfiecDns.cfiec_dns.tasks.crontab_core_indicator',
+        'schedule': crontab(minute=10),
+        'args': []
+    },
+}
