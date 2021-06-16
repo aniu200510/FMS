1834170c8b345823372b712836a283a5ed0873cd
zyli <zyli@cfiec.net>
2021-06-16 17:06:23 +0800

    refactor:Optimize the installation script.

--- install.sh
+++ install.sh
@@ -4,6 +4,42 @@
 #  Funding management system installation instructions.
 #
 
+pro_root='/opt/cfiec/fms/'
+pro='/opt/cfiec/'
+python_home=`which python3`
+
+
+initPro(){
+	if [ ! -d ${pro} ];
+	then
+		sudo mkdir -p ${pro} >/dev/null
+	fi
+	sudo chown ${who}.${who} ${pro}
+	if [ ! -d ${pro_root} ];
+	then
+		mkdir -p ${pro_root} >/dev/null 
+	fi
+}
+
+env_virtual(){
+    if [ ! -d ${pro_root}.venv ];
+	then
+		mkdir ${pro_root}.venv >/dev/null 
+	fi
+	sudo apt-get install -y python3-venv >/dev/null
+	${python_home} -m venv $pro_root.venv >/dev/null  #必须是python3才支持venv
+    ${pro_root}.venv/bin/pip install billiard==3.5.0.5 >/dev/null 2>&1
+    ${pro_root}.venv/bin/pip install coverage >/dev/null 2>&1
+	${pro_root}.venv/bin/pip install -r ./requirements/requirements.txt
+	${pro_root}.venv/bin/pip install -r ./requirements/requirements-pyecharts.txt
+}
+
+
+cpPy() {
+    # rsync all source code
+    rsync -avz  --exclude .git/ --exclude tmp/ . ${pro_root}
+}
+
 create_db(){
     sudo -u postgres psql -c 'CREATE DATABASE "fms";'
     sudo -u postgres psql -c "CREATE USER funder WITH PASSWORD 'funder1017';"
@@ -14,4 +50,15 @@ create_db(){
     sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE "fms" TO funder;'
 }
 
+celery(){
+    sudo cp ./etc/fund.service /etc/systemd/system/ >/dev/null
+    sudo systemctl daemon-reload
+    sudo systemctl enable celery >/dev/null 2>&1
+    sudo systemctl restart celery >/dev/null 
+}
+
+initPro
+cpPy
+env_virtual
 create_db
+celery
